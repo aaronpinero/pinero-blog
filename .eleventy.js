556133c5e78e5baca9ce20227ae2e9c1b9af7746
Aaron Pinero <aaronpinero@gmail.com>
2020-07-08 12:41:19 -0400

    adding fontawesome

--- .eleventy.js
+++ .eleventy.js
@@ -15,4 +15,5 @@ module.exports = function (eleventyConfig) {
     return coll;
   });
   eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("fontawesome-subset");
 };
---
+++ fontawesome-subset/js-packages/@fortawesome/pro-regular-svg-icons/index.d.ts
@@ -0,0 +1,6 @@
+export const faAngleRight: IconDefinition;
+export const faLightbulbOn: IconDefinition;
+import { IconDefinition, IconLookup, IconName, IconPrefix, IconPack } from '@fortawesome/fontawesome-common-types';
+export { IconDefinition, IconLookup, IconName, IconPrefix, IconPack } from '@fortawesome/fontawesome-common-types';
+export const prefix: IconPrefix;
+export const far: IconPack;
