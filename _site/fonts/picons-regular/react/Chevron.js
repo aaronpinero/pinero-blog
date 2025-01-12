9330d4fe0185b5f77772b6fee4ef3bf049183a09
Aaron Pinero <aaronpinero@gmail.com>
2025-01-11 20:40:56 -0500

    rebuild of site

--- _site/fonts/picons-regular/react/Chevron.js
+++
@@ -1,6 +0,0 @@
-
-import React from 'react';
-
-export const Chevron = props => (
-  <svg viewBox="0 0 20 20" {...props}></svg>
-);
--- _site/fonts/picons-regular/react/Lock.js
+++
@@ -1,6 +0,0 @@
-
-import React from 'react';
-
-export const Lock = props => (
-  <svg viewBox="0 0 20 20" {...props}><path d="M44,18 C44,11.552 38.518,6 32,6 C25.480,6 20,11.311 20,18 L20,20 L14,20 L14,18 C14,8.064 22.064,0 32,0 C41.739,0 50,8.128 50,18 L50,30 L56,30 L56,64 L8,64 L8,30 L44,30 L44,18 Z M50,36 L14,36 L14,58 L50,58 L50,36 Z" fillRule="evenodd" /></svg>
-);
