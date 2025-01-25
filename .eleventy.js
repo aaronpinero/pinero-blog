3312c1d37e0b07a92db8d5b197975f56b3b8d36f
Aaron Pinero <aaronpinero@gmail.com>
2025-01-25 11:22:56 -0500

    adding RSS feed for site

--- .eleventy.js
+++ .eleventy.js
@@ -3,11 +3,17 @@ const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 moment.locale('en');
 
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addShortcode('now', () => {
+    return new Date().toUTCString();
+  });
   eleventyConfig.addFilter('dateIso', date => {
     return moment(date).toISOString();
   });
   eleventyConfig.addFilter('dateReadable', date => {
     return moment(date).utc().format('LL'); // E.g. May 31, 2019
+  });
+  eleventyConfig.addFilter('dateFeed', date => {
+    return moment(date).utc();
   });
   eleventyConfig.addCollection('postArchive', function(collectionApi) {
     var coll = collectionApi.getFilteredByTag("post");
